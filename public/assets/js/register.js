6a811fbf33a4a9ea6ce8a7dfb8ff7c9469c54902
KaliBlip <69836195+KaliBlip@users.noreply.github.com>
2024-09-18 21:25:37 +0000

    "Removed import of 'firebase.js' and added no new imports."

--- public/assets/js/register.js
+++ public/assets/js/register.js
@@ -1,4 +1,4 @@
-import { initializeApp } from 'firebase.js';
+
 import { getFirestore, doc, setDoc, collection, addDoc } from "https://www.gstatic.com/firebasejs/9.20.0/firebase-firestore.js";
 import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.20.0/firebase-auth.js";
 
